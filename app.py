3d0555c40fd504652b40041315b34b3ef05e63c1
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-06-27 18:14:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ model_path = 'oneone.keras'
 model_loaded = False
 try:
     cnn_model = tf.keras.models.load_model(model_path)
+    print(cnn_model.summary())
+
     model_loaded = True
 except FileNotFoundError:
     st.error(f"CNN model file '{model_path}' not found. Please ensure the file exists.")
 except Exception as e:
     st.error(f"An unexpected error occurred: {e}")
 
-print(cnn_model.summary())
