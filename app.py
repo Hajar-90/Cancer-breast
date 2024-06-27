08f037c367affab11c0ef66c7472027e181c9d77
Hajar-90 <147998132+Hajar-90@users.noreply.github.com>
2024-06-27 17:51:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import streamlit as st
 import tensorflow as tf
 
-# Absolute path to the .keras file
-model_path = os.path.join(os.getcwd(), 'oneone.keras')
+# Define absolute path to the .keras file
+model_path = '/mount/src/Cancer-breast/oneone.keras'
 
 # Load CNN model with error handling
 model_loaded = False
@@ -11,13 +11,7 @@ try:
     cnn_model = tf.keras.models.load_model(model_path)
     model_loaded = True
 except FileNotFoundError:
-    st.error(f"CNN model file '{model_path}' not found. Please upload the model file.")
+    st.error(f"CNN model file '{model_path}' not found. Please ensure the file exists.")
 except Exception as e:
     st.error(f"An unexpected error occurred: {e}")
 
-
-my_list = []
-if my_list:
-    element = my_list.pop()
-else:
-    print("List is empty, cannot pop.")
